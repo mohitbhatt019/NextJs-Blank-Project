63e3d442b0b21bd518f6109f08efe34ad82c7ec0
Mohitbhatt <mohitbhatt@gmail.com>
2023-10-11 18:15:05 +0530

    css Implemented

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,3 +1,9 @@
-export default function(){
-    
+export default function () {
+  return (
+    <>
+      <h1 style={{ backgroundColor: "blue" }}> Inline CSS</h1>
+      <h3>Welcome to Next JS</h3>
+      <h2>Blue change</h2>
+    </>
+  );
 }
