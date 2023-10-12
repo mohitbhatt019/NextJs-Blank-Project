01131b33a3ab25d410424b8860986fea9187e254
Mohitbhatt <mohitbhatt@gmail.com>
2023-10-12 18:14:48 +0530

    Dynamic metadata implemented

---
+++ src/app/students/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <div>
+        <title>Student page</title>  
+        {/* //Wrong wayt to implement metadata, it show 2 title in metadata nut impact SEO */}
+        <h1>Student Page</h1>
+      </div>
+    );
+  }
---
+++ src/app/user/page.js
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>User Page</h1>
+    </div>
+  );
+}
+
+export function generateMetadata() {
+  return{
+    title:"User page titleee",
+    description:"User page description"
+  }
+}
